3f8d39c2f7238f58cdb048da1b5edde8c0c1ae9d
Nick Stovall <nsto@users.noreply.github.com>
2022-03-11 08:20:36 -0800

    Delete pbsbackup.sh

--- pbsbackup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export PBS_REPOSITORY=root@pam!gnar-kill@pbs.nrstovall.com:ds220p
-export PBS_PASSWORD=423ef28b-cfb0-416c-9874-8a28232c6a76
-
-proxmox-backup-client backup root.pxar:/ home.pxar:/home
